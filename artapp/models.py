b8be9ab3a8b42299b6d2a1ff88c4e2937f9aee73
disen <610039018@qq.com>
2018-07-03 15:27:13 +0800

    实现分类的CRUD

--- artapp/models.py
+++ artapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# 小说或文章标签类型的模型
 class ArtTag(models.Model):
     title = models.CharField(max_length=20,
                              verbose_name='作品类别',
