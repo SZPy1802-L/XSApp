09fa11ffaec00c480b07f35e68b0232f51b9e8b3
laoliao <liao@1000phone.com>
2018-07-03 17:34:00 +0800

    新建文章模型,修改,删除,首页文章显示

--- artapp/urls.py
+++ artapp/urls.py
@@ -1,8 +1,12 @@
 
 from django.conf.urls import url
 from django.contrib import admin
-from artapp import views
+from artapp.views import *
+
 urlpatterns = [
     # 声明主页面的请求
-    url('', views.index),
+    url('^$', index),
+    url(r'^tags$',add_tags),
+    url(r'^delete_tag$',delete_tag),
+    url(r'^list_tags$',list_tags)
 ]
--- artapp/views.py
+++ artapp/views.py
@@ -1,21 +1,60 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from artapp.models import  ArtTag,Art
 # Create your views here.
 
 
 def index(request):
-    # 请求路径和请求方法
-    print(request.path,request.method)
-    # 请求头的源信息和GET请求参数(查询参数)
-    # print(request.META,request.GET) # 字典类型
-    print(request.GET)
-    print(request.GET.get('page'),request.GET.get('tag'))
-    # POST请求参数,表单参数
-    print(request.POST) # 字典类型
 
-    # 响应对象
-    # return HttpResponse('<h1>您好</h1>')
-    # return JsonResponse({'name':'Disen'})
-    return render(request,'art/list.html',context = {'name':'Disen','age':20})
-    # 第一次修改注释
+    return render(request,'art/list.html',context = {'arts':Art.objects.all(),
+                                                     'tags':ArtTag.objects.all()})
+
+
+
+def add_tags(request):
+    # 第一次跳转,编辑
+    if request.method == 'GET':
+        # 新增编辑(不带tag的id),修改编辑(带tag的id) ,
+        # 判断是否为修改
+        id = request.GET.get('id')
+        # print(id)  # 1
+        # print(type(id))  # str
+        tag = None
+        if id :
+            # 存在id,即为修改
+            tag = ArtTag.objects.get(id=id)
+        return render(request,'art/edit_tags.html',{'tag':tag})
+
+    # 第二次跳转,为表单的post提交,新增修改
+    else:
+        # post 请求
+        # 新增 和 修改
+        if request.POST.get('id'):
+            tag = ArtTag.objects.get(id = request.POST.get('id'))
+        else:
+            tag = ArtTag()
+        tag.title = request.POST.get('title')
+        tag.save()  # 保存到数据库
+        return redirect('/art/list_tags')  # 重定向
+
+
+def delete_tag(request):
+    id = request.GET.get('id')
+    if id:
+        result = ArtTag.objects.filter(id=id)
+        # print(result)  # <QuerySet [<ArtTag: ArtTag object>]>
+        # print(type(result))  # <class 'django.db.models.query.QuerySet'>
+        # 判断查询集是否存在
+        if result.exists():
+            result.delete()
+    # 重定向到列表页面
+    return redirect('/art/list_tags')
+
+
+def list_tags(request):
+    return render(request,'art/tags_list.html',
+                  context={'tags':ArtTag.objects.all()})
+
+
+
+
