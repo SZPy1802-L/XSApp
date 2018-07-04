2066345f781736c61af07a9b274dc4f1da784114
disen <610039018@qq.com>
2018-07-04 16:33:39 +0800

    修改了搜索样式

--- artapp/urls.py
+++ artapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^list_tags', list_tags),
     url(r'^delete_tag', delete_tag),
     url(r'^art_edit', views_art.art_edit),  # 编辑文章
+    url(r'^search', views_art.search),  # 搜索文章
     url(r'^', index),
 
 ]
