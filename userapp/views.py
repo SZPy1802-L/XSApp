318c88d09b3e50118606b8320ce601d287424334
disen <610039018@qq.com>
2018-07-05 11:58:16 +0800

    实现用户注册和验证

--- userapp/views.py
+++ userapp/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from userapp.forms import UserForm
+from userapp.models import UserProfile
+from json import loads
 
 # Create your views here.
 def regist(request):
-    return render(request, 'user/regist.html')
+    if request.method == 'GET':
+        return render(request, 'user/regist.html')
+    else: # post
+        # user = UserProfile()
+        # user.username = request.POST.get('username')
+        # user.password = request.POST.get('password')
+        # user.phone = request.POST.get('phone')
+        # user.photo = request.FILES.get('photo')
+
+        # 创建UserForm的实例对象
+        # 要求：表单中指定的字段名必须和model模型中字段名保持一致
+        userForm = UserForm(request.POST,
+                            request.FILES)
+
+        # 保存之前要验证数据
+        if userForm.is_valid():  # 验证通过
+            userForm.save()
+            return redirect('/art/')
+        else:
+            print('验证出错：', userForm.errors.as_json())
+
+            # form.errors.as_json() 返回是 json字符串
+            # 格式： {'字段名1':[{'message':'xx', code:'required'}],
+            #        '字段名2':[{'message':'xx', code:'required'}]}
+            return render(request, 'user/regist.html',
+                          {'errors': loads(userForm.errors.as_json())})
+
