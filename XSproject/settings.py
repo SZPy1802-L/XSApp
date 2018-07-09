77d3ee7c9393257a2a6d7a3e20d9587a591c4e33
disen <610039018@qq.com>
2018-07-09 12:07:22 +0800

    引入xadmin和ueditor富本

--- XSproject/settings.py
+++ XSproject/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# 引入sources root目录
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(1, os.path.join(BASE_DIR, 'extapps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -31,7 +35,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
+    'xadmin',
+    'crispy_forms',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -39,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'artapp',
     'userapp',
+    'DjangoUeditor',
+
 ]
 
 MIDDLEWARE = [
@@ -106,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -130,6 +138,9 @@ STATICFILES_DIRS = [
 
 # 多媒体文件（图片、视频、音频、表格等文件）
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/ups')
+
+# 多媒体文件资源请求的路径位置
+MEDIA_URL = 'ups/'
 
 
 # 配置session方案(默认存在数据库中)
